c67d64d4deb77ef751e523344ee4e8f9d55ba97f
Jeb Cui <jebcui99@gmail.com>
2020-12-13 15:03:30 -0500

    ADd Access-Control-Allow-Origin

--- python.js
+++ python.js
@@ -2,6 +2,12 @@ let {PythonShell} = require('python-shell')
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/:information', (req, res) => {
     var information_array = (""+req.params.information).split(", ");
     console.log(""+req.params.information);
