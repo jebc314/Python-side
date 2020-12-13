2f0521ebbcf146a28e723e04f2b4c8dcb8266ba5
Jeb Cui <jebcui99@gmail.com>
2020-12-13 14:52:38 -0500

    More fix

--- python.js
+++ python.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
 app.get('/:information', (req, res) => {
-    var information_array = (""+req.params.information).split(" ");
+    var information_array = (""+req.params.information).split(", ");
     console.log(""+req.params.information);
     var int_array = [];
 
