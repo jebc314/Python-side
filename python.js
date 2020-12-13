9f4f11f801fb28fe381ef0e51403a78d8067ca07
Jeb Cui <jebcui99@gmail.com>
2020-12-13 16:29:22 -0500

    back to original

--- python.js
+++ python.js
@@ -3,12 +3,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
 app.use(function(req, res, next) {
-  //res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*");
   //res.header("Access-Control-Allow-Origin", "X-Requested-With");
-  response.setHeader("Access-Control-Allow-Origin", "*");
-response.setHeader("Access-Control-Allow-Credentials", "true");
-response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
 });
 
