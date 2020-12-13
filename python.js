06369029957a766159b63138f5e043379bea8162
Jeb Cui <jebcui99@gmail.com>
2020-12-13 15:05:53 -0500

    FIX

--- python.js
+++ python.js
@@ -4,7 +4,7 @@ const app = express()
 const port = process.env.PORT || 8080
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Origin", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Origin", "X-Requested-With");
   next();
 });
 
